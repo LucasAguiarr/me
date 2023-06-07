a093755573357bf0c6f60c70aa35452c2635dae1
Lucas Aguiar <luqkinhaas@gmail.com>
2023-06-07 17:52:14 -0300

    fix: correct the classname in the nav

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -7,7 +7,7 @@ import { ProfileCard } from '@/components/profile-card';
 
 export function SiteHeader() {
   return (
-    <header className="fixed w-full h-16 top-0 z-40  border-b bg-background/95 backdrop-blur md:border-r md:border-b-0md:w-64 md:h-screen">
+    <header className="fixed w-full h-16 top-0 z-40 border-b bg-background/95 backdrop-blur md:border-r md:border-b-0 md:w-64 md:h-screen">
       <div className="flex h-full items-center justify-between px-4 md:justify-start md:flex-col md:py-10">
         <Link href="/">
           <ProfileCard />
