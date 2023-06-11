e7ed6b29eb312c47796af9bad423961faab5b993
Lucas Aguiar <luqkinhaas@gmail.com>
2023-06-10 23:05:28 -0300

    feat: add project description

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,7 +7,7 @@ export default function ProjectsPage() {
     <div>
       <PageHeader
         title={['Projects']}
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit"
+        description="Explore my latest projects and witness innovation in action."
       />
 
       <div className="mt-16 mb-10 flex flex-col gap-4">
