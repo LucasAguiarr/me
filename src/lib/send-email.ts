dceed03c235144f5b131855d68199b365e100ef0
Lucas Aguiar <luqkinhaas@gmail.com>
2023-06-15 15:37:21 -0300

    fix: remove console log

--- src/lib/send-email.ts
+++ src/lib/send-email.ts
@@ -1,6 +1,5 @@
 export async function SendEmail(data: SendEmail) {
   const { from, to, subject, html } = data;
-  console.log('Sending email', data);
   await fetch('/api/send', {
     method: 'POST',
     body: JSON.stringify({ from, to, subject, html }),
